ddcf900852b5b828c1154e823ef76186ff1caab7
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2024-01-23 21:55:10 -0300

    Atualização

---
+++ C# e Programação Orientada a Objetos Direto ao Ponto/POODiretoAoPonto/WorkingWithString/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkingWithString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
