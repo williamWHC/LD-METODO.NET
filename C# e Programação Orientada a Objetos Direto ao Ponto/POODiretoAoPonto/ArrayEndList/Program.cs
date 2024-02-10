b26fccfaccf9c38aa0a4b9844c630283536075fb
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2024-02-10 13:30:04 -0300

    :construuctions; Working with list  and array

--- C# e Programação Orientada a Objetos Direto ao Ponto/POODiretoAoPonto/ArrayEndList/Program.cs
+++ C# e Programação Orientada a Objetos Direto ao Ponto/POODiretoAoPonto/ArrayEndList/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ArrayEndList
+﻿
+using System.Net.Http.Headers;
+
+namespace ArrayEndList
 {
     internal class Program
     {
@@ -39,6 +42,54 @@
             #endregion
 
             #region List
+
+            //---------- Adicionar a lista.---------- >
+
+            List<int> numeros = new List<int>() { 0, 1, 2, 3, 4 };
+            var listNumeros = new List<int>() { 9, 10};
+            List<int> ints = new List<int>();
+
+            numeros.Add(5);
+
+            int[] intArray = new int[] { 6, 7, 8 };
+            numeros.AddRange(intArray);
+
+            numeros.AddRange(listNumeros);
+
+            //---------- Desordenação ---------- >
+            Console.WriteLine("Lista reversa.");
+            numeros.Reverse();
+            numeros.ForEach(w =>  Console.WriteLine(w));
+
+            //---------- Ordenação ---------- >
+            Console.WriteLine("Lista ordenada");
+            numeros.Sort();
+            numeros.ForEach(w => Console.WriteLine(w));
+            var resultFind = numeros.Find(w => w == 0);
+
+            //---------- validação ---------- >
+            var quantidadeElementos = numeros.Count;
+            var bYesContains = numeros.Contains(14); //false
+            var bNoContains = numeros.Contains(2); //True
+
+            numeros.ForEach(i => i.CompareTo(3));
+            var teste = numeros[0].CompareTo(3);
+
+            //---------- Remoção de item ---------- >
+            numeros.Remove(4);
+            numeros.RemoveAll(w => w == 0);
+            numeros.RemoveRange(1, 2); //primeiro parametro é o index do array e o segundo e quantos posições do array ira ser removido.
+            numeros.Clear();
+
+            Console.WriteLine(numeros);
+
+            //[] - Adicionar list a lista.
+            //[] - Desordenar lista
+            //[] - Ordenar lista
+            //[] - Valida se contem valor na lista.
+            //[] - Remover item da lista
+            //[] - Limpar toda a lista.
+
 
             #endregion
 
